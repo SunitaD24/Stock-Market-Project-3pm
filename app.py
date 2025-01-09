160290399636392e5791e1538d2e2da330c847b1
SunitaD24 <dsunita.dange@gmail.com>
2025-01-09 11:06:25 +0000

    util

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
 import streamlit as st
+from utils import StockAPI
+
+
+def get_stock_api():
+    return StockAPI
+
+client = get_stock_api()
+
+def search_company(company):
+    return client.search_symbol(company)
+
+
+
+#set the header title
+st.set_page_config(page_title="Stock Market App")
